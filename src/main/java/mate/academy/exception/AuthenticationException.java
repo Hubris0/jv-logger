23058da8ab132053b8c0c61062ccc26535339105
user <pawel.m.janik@gmail.com>
2026-08-16 16:20:43 +0200

    Removed unused imports and code formatting

--- src/main/java/mate/academy/exception/AuthenticationException.java
+++ src/main/java/mate/academy/exception/AuthenticationException.java
@@ -1,8 +1,5 @@
 package mate.academy.exception;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public class AuthenticationException extends Exception {
 
     public AuthenticationException(String message) {
